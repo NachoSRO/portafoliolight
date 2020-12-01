1b69442db3b8c171bbc697dfc5508d09c57fface
NachoSRO <ign.spr@live.com>
2020-11-30 21:20:21 -0300

    Test

--- portafolio/cliente/models.py
+++ portafolio/cliente/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Cliente(models.Model):
     rut = models.CharField(unique=True, max_length=12)
-    nombre = models.CharField(max_length=120)
+    nombre = models.CharField(max_length=125)
     telefono = models.IntegerField()
     
 
